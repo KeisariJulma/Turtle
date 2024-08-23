3bf8affcc67079a4b7886a95208d68f244ec34e9
Antti-Jussi Oksa <50314142+KeisariJulma@users.noreply.github.com>
2024-08-23 23:56:48 +0300

    d

--- mining/strip.lua
+++ mining/strip.lua
@@ -1,13 +1,17 @@
+-- Example Lua script for CC: Tweaked
 
-local slot = {
-    fuel = 1,
-    torch = 2,
-    fill =3
-}
-
-
-
-function Main(testTxt)
-    print(testTxt)
-    print("alsop")
+-- Function to ask for a parameter and return the value
+function askParameter(prompt)
+    -- Display the prompt to the user
+    print(prompt)
+    
+    -- Read user input
+    local input = read()
+    
+    -- Return the input value
+    return input
 end
+
+-- Example usage
+local paramName = askParameter("Please enter the name of the parameter:")
+print("You entered: " .. paramName)
