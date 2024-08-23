bcd0cc3745ed8a6f7d55447fc1460b9d7c4f8d2e
Antti-Jussi Oksa <50314142+KeisariJulma@users.noreply.github.com>
2024-08-23 23:48:05 +0300

    Updater ficx

--- update/update.lua
+++ update/update.lua
@@ -1,13 +1,18 @@
-local url =
-"https://raw.githubusercontent.com/KeisariJulma/Turtle/master/mining/strip.lua"
+local url = "https://raw.githubusercontent.com/KeisariJulma/Turtle/master/mining/strip.lua"
 local response = http.get(url)
+
 if response then
     local r = response.readAll()
     response.close()
-    local file = fs.open("strip", "w")
+    
+    -- Extract file name from the URL
+    local fileName = url:match("^.+/(.+)$") or "downloaded_file"
+    
+    local file = fs.open(fileName, "w")
     file.write(r)
     file.close()
-    print("Success")
+    
+    print("Success: File '" .. fileName .. "' has been downloaded.")
 else
-    print("Failure")
+    print("Failure: Could not fetch the file from the URL.")
 end
