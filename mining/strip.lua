dcf6cfa490d55dda828bd232e5d19f7187a71142
Antti-Jussi Oksa <50314142+KeisariJulma@users.noreply.github.com>
2024-08-23 13:57:02 +0300

    cords

--- mining/strip.lua
+++ mining/strip.lua
@@ -1,1 +1,31 @@
+local slot = {
+    fuel = 1,
+    torch = 2,
+    fill = 3
+}
+local torchInterval = 8
 
+local brachStart = {
+    x = turtle.getX(),
+    y = turtle.getY(),
+    z = turtle.getZ()
+
+}
+
+
+function Main(amountOfBranches, Brancelength, brancHeight)
+    print("No")
+end
+
+function torch()
+    turtle.select(slot.torch)
+    turtle.placeDown()
+end
+
+
+function printPosition()
+    print("Hello")
+    print(brachStart.x, brachStart.y, brachStart.z) 
+end
+
+printPosition()
